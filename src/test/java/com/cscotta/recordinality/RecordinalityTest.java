636139588a57a68e352afe393b7463fc38aaea70
C. Scott Andreas <scott@paradoxica.net>
2013-08-19 14:22:33 -0700

    Add commons-math3 and commons-io to pom.

---
+++ src/test/java/com/cscotta/recordinality/RecordinalityTest.java
@@ -0,0 +1,11 @@
+package com.cscotta.recordinality;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: cscotta
+ * Date: 8/19/13
+ * Time: 10:27 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class RecordinalityTest {
+}
